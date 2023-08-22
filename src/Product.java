93bc0d26f512c0cfcc78d586d19d0e68072b49ee
Carlos Jorge Avila <carlos_jorge_avila@epam.com>
2023-08-22 10:47:05 +0200

    add Product class

---
+++ src/Product.java
@@ -0,0 +1,11 @@
+public class Product {
+
+    private String name;
+    private String type;
+    private int quantity;
+    public Product(String name, String type, int quantity) {
+        this.name=name;
+        this.type=type;
+        this.quantity=quantity;
+    }
+}
